a313b8a00ebdd6fee0c3fbdbfc78bf1f3e0c6c09
Arielmtts7 <arielmattosnev@gmail.com>
2023-03-09 09:18:39 -0300

    Insert project on github

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom'
+import Newproject from './components/pages/Newproject'
+import Projects from './components/pages/Projects'
+import Contact from './components/pages/Contact'
+import Company from './components/pages/Company'
+import Home from './components/pages/Home'
+import Container from './components/layout/Container'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import Project from './components/pages/Project'
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+
+      <Container customClass="min-height">
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/projects" element={<Projects />} />
+
+        <Route path="/company" element={<Company />} />
+
+        <Route path="/contact" element={<Contact />} />
+
+        <Route path="/newproject" element={<Newproject />} />
+
+        <Route path="/project/:id" element={<Project />} />
+      </Routes>
+      </Container>
+      <Footer />
+    </Router>
+  )
+}
+
+export default App
